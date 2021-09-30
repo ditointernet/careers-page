be43e2d83438e5559899a4494efe91a0f8d3b5ff
Uriell <me+github@uriell.dev>
2021-09-29 22:13:50 -0300

    adding parallax effect for brand logos

---
+++ sections/Scale/Brands.tsx
@@ -0,0 +1,59 @@
+import styled from "styled-components";
+import { COLORS, GRID } from "@ditointernet/uai-foundation";
+import { ParallaxProvider, Parallax } from "react-scroll-parallax";
+
+const BrandLogo = styled.div`
+  width: ${GRID(8)};
+  height: ${GRID(8)};
+  display: inline-block;
+  background-color: ${COLORS.GREEN_MAIN};
+`;
+
+const Row = styled.div`
+  display: flex;
+
+  ${BrandLogo} ~ ${BrandLogo} {
+    margin-left: ${GRID(13)};
+  }
+`;
+
+const BrandsRow = () => (
+  <Row>
+    <BrandLogo />
+    <BrandLogo />
+    <BrandLogo />
+    <BrandLogo />
+    <BrandLogo />
+    <BrandLogo />
+    <BrandLogo />
+    <BrandLogo />
+    <BrandLogo />
+  </Row>
+);
+
+const BrandsContainer = styled.div`
+  white-space: nowrap;
+  display: flex;
+  flex-flow: column;
+  align-items: center;
+
+  ${Row}:last-of-type {
+    margin-bottom: ${GRID(8)};
+  }
+`;
+
+const Brands = () => (
+  <BrandsContainer>
+    <h2>Brands</h2>
+    <ParallaxProvider>
+      <Parallax x={[-100, 0]}>
+        <BrandsRow />
+      </Parallax>
+      <Parallax x={[75, 0]}>
+        <BrandsRow />
+      </Parallax>
+    </ParallaxProvider>
+  </BrandsContainer>
+);
+
+export default Brands;
---
+++ sections/index.tsx
@@ -0,0 +1,15 @@
+export const Navbar = () => <h2>Navbar</h2>;
+export const Hero = () => <h2>Hero</h2>;
+export const Objective = () => <h2>Objective</h2>;
+export { default as Scale } from "./Scale";
+export const Tech = () => <h2>Tech</h2>;
+export const Team = () => <h2>Team</h2>;
+export const Values = () => <h2>Values</h2>;
+export const Testimonials = () => <h2>Testimonials</h2>;
+export const Areas = () => <h2>Areas</h2>;
+export const Reasons = () => <h2>Reasons</h2>;
+export const Initiatives = () => <h2>Initiatives</h2>;
+export const Benefits = () => <h2>Benefits</h2>;
+export const Screening = () => <h2>Screening</h2>;
+export const Jobs = () => <h2>Jobs</h2>;
+export const Connect = () => <h2>Connect</h2>;
