5ba82f43bd0959993a8f3451def83c86c4777040
Igor Gottschalg <igorgsduarte@gmail.com>
2021-10-14 17:54:14 -0300

    FIx mobile check

--- components/Brands.tsx
+++ components/Brands.tsx
@@ -22,7 +22,7 @@ const Brands = () => {
     return Boolean(
       window?.navigator.userAgent.match(
         /Android|BlackBerry|iPhone|iPad|iPod|Opera Mini|IEMobile|WPDesktop/i
-      )
+      ) || window.innerWidth < 768
     );
   }, []);
 
