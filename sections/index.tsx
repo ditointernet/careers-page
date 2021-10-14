dd21e9593f01a662e6c6819697ff6a8ae009e283
Diego Roso da Silva <diego.silva@dito.com.br>
2021-10-14 12:32:37 -0300

    apply effect on navbar

--- sections/index.tsx
+++ sections/index.tsx
@@ -1,8 +1,16 @@
-export const Navbar = () => <h2>Navbar</h2>;
-export const Hero = () => <h2>Hero</h2>;
+export { default as Navbar } from "./Navbar";
+export const Hero = () => (
+    <section id="sobre-a-dito">
+        <h2>Hero</h2>
+    </section>
+);
 export const Objective = () => <h2>Objective</h2>;
 export { default as Scale } from "./Scale";
-export const Tech = () => <h2>Tech</h2>;
+export const Tech = () => (
+    <section id="nossas-tecnologias">
+        <h2>Tech</h2>
+    </section>
+);
 export const Team = () => <h2>Team</h2>;
 export const Values = () => <h2>Values</h2>;
 export const Testimonials = () => <h2>Testimonials</h2>;
@@ -11,5 +19,9 @@ export const Reasons = () => <h2>Reasons</h2>;
 export const Initiatives = () => <h2>Initiatives</h2>;
 export const Benefits = () => <h2>Benefits</h2>;
 export const Screening = () => <h2>Screening</h2>;
-export const Jobs = () => <h2>Jobs</h2>;
+export const Jobs = () => (
+    <section id="nosso-processo-seletivo">
+        <h2>Jobs</h2>
+    </section>
+);
 export const Connect = () => <h2>Connect</h2>;
