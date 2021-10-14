51c4cfaf1f8dc73bf9139ea6e99b2d252ff2553f
Diego Roso da Silva <diego.silva@dito.com.br>
2021-10-14 17:16:31 -0300

    use uai-foundation where necessary

--- components/ButtonPrimary/ButtonPrimary.tsx
+++ components/ButtonPrimary/ButtonPrimary.tsx
@@ -1,20 +1,21 @@
 import styled from 'styled-components'
+import { COLORS, GRID } from "@ditointernet/uai-foundation";
 
 const ButtonPrimary = styled.a`
-    padding: 0;
-    background-color: rgb(0 189 106);
+    padding: ${GRID(0)};;
+    background-color: ${COLORS.GREEN_MAIN};
     outline: none;
-    border: 0;
-    border-radius: 56px;
-    color: #fff;
+    border: ${GRID(0)};;
+    border-radius: ${GRID(7)};
+    color: ${COLORS.WHITE};
     position: relative;
     cursor: pointer;
     overflow: hidden;
-    font-size: 16px;
+    font-size: ${GRID(2)};
     font-weight: 500;
     display: flex;
-    min-width: 190px;
-    min-height: 48px;
+    min-width: ${GRID(23.75)};;
+    min-height: ${GRID(6)};
     display: flex;
     align-items: center;
     justify-content: center;
