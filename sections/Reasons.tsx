ea0b8e7596d1c75e5319bb24888510c4180e2e4a
Igor Gottschalg <igorgsduarte@gmail.com>
2021-10-15 18:54:02 -0300

    Add block style

--- sections/Reasons.tsx
+++ sections/Reasons.tsx
@@ -4,12 +4,44 @@ import { COLORS, GRID } from "@ditointernet/uai-foundation";
 import RemoteJob from "../public/images/remote-job.svg";
 import Location from "../public/images/location.svg";
 
+const reasonsBlocks = [
+  {
+    image: "/images/gptw.jpg",
+    text: "Desde 2017, estamos entre as melhores empresas para trabalhar (GPTW).",
+  },
+  {
+    image: "/images/time.svg",
+    text: "Time formado por mais de 100 FERAS de todo o Brasil.",
+  },
+  {
+    image: "/images/flex-worksheet.svg",
+    text: "Horários flexíveis de verdade, sem essa de bater ponto por aqui.",
+  },
+  {
+    image: "/images/career-map.svg",
+    text: "Ajudamos a desenvolver seu mapa de carreira para você seguir crescendo.",
+  },
+  {
+    image: "/images/coop.svg",
+    text: "Espírito colaborativo. Aqui não tem concorrência, tem parceria.",
+  },
+  {
+    image: "/images/performance.svg",
+    text: "Realizamos ciclos de performance periódicos.",
+  },
+];
+
 const Reasons = () => (
   <Section>
     <Title>Mais razões para trabalhar na Dito</Title>
 
     <SlideBlocks>
-      <Block></Block>
+      {reasonsBlocks.map((block) => (
+        <Block>
+          <img src={block.image} />
+          <p>{block.text}</p>
+        </Block>
+      ))}
     </SlideBlocks>
 
     <Content>
@@ -63,8 +95,36 @@ const Title = styled.h2`
   }
 `;
 
-const SlideBlocks = styled.div``;
-const Block = styled.div``;
+const SlideBlocks = styled.div`
+  display: flex;
+  overflow-x: scroll;
+  flex-wrap: nowrap;
+  margin-left: ${GRID(9)};
+`;
+const Block = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  padding: ${GRID(4)};
+  width: ${GRID(47.5)};
+  border-radius: ${GRID(2)};
+
+  &:hover {
+    box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.08);
+  }
+
+  img {
+    margin-bottom: ${GRID(4)};
+    width: ${GRID(12)};
+    height: auto;
+  }
+
+  p {
+    color: ${COLORS.GRAY_4};
+    font-size: ${GRID(2)};
+    font-weight: normal;
+  }
+`;
 
 const Columns = styled.div`
   display: flex;
