551cf722ae8bc04224485404d07940ed25911574
roger067 <rosestolato.rogerio@gmail.com>
2021-10-15 16:37:43 -0300

    some final adjusts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -67,7 +67,10 @@ function MyApp({ Component, pageProps }: AppProps) {
           content="Nossa paixão por tecnologia está revolucionando o varejo. Vem com a gente!"
         />
         <link rel="icon" href="/favicon.ico" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0"
+        />
       </Head>
       <GlobalStyle />
       <Component {...pageProps} />
