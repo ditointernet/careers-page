16e5649ac0a5c38f55b97cb3689ee93d0d6aefec
Diego Roso da Silva <diego.silva@dito.com.br>
2021-10-19 18:39:20 -0300

    fix width image

--- sections/Benefits/Benefits.tsx
+++ sections/Benefits/Benefits.tsx
@@ -93,7 +93,7 @@ const Section = styled.section`
     @media only screen and (min-width: 1024px) {
         margin: 0 auto;
         padding: ${GRID(10)} ${GRID(9)} ${GRID(25)};
-        background-size: auto;
+        background-size: ${GRID(53.75)};
         background-position-x: 85%;
     }
 `
