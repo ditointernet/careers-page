2d4d4d6c6665c05f4c77cd7e66199e89bde65db5
Diego Roso da Silva <diego.silva@dito.com.br>
2021-10-20 16:08:12 -0300

    create CardsList component

--- sections/Benefits/Benefits.tsx
+++ sections/Benefits/Benefits.tsx
@@ -1,11 +1,7 @@
 import React from "react";
 import styled from "styled-components"
+import CardsList from "@/components/CardsList";
 import { COLORS, GRID } from "@ditointernet/uai-foundation";
-import { Swiper, SwiperSlide } from "swiper/react";
-import SwiperCore, { Navigation } from "swiper";
-
-import "swiper/css";
-import "swiper/css/navigation"
 
 type Card = {
     img: string,
@@ -14,8 +10,6 @@ type Card = {
 }
 
 const Benefits = () => {
-    SwiperCore.use([Navigation]);
-
     const cards: Card[] = [
         {
             img: "./icons/plano-de-saude.svg",
@@ -49,7 +43,7 @@ const Benefits = () => {
         },
         {
             img: "./icons/bonus-anual.svg",
-            title: "Bonûs anual",
+            title: "Bônus anual",
             description: "Bônus anual por atingimento de metas, afinal, esses são os resultados do seu trabalho.",
         },
     ];
@@ -57,24 +51,7 @@ const Benefits = () => {
     return (
         <Section>
             <Title>Nossos benefícios</Title>
-            <SwiperMobile navigation={true} loop={true}>
-                {cards.map((card: Card, index: number) => (
-                    <SwiperSlide key={index}>
-                        <CardImg style={{ backgroundImage: `url(${card.img})` }} />
-                        <CardTitle>{card.title}</CardTitle>
-                        <CardDescription>{card.description}</CardDescription>
-                    </SwiperSlide>
-                ))}
-            </SwiperMobile>
-            <WrapperCards>
-                {cards.map((card: Card, index: number) => (
-                    <Card key={index}>
-                        <CardImg style={{ backgroundImage: `url(${card.img})` }} />
-                        <CardTitle>{card.title}</CardTitle>
-                        <CardDescription>{card.description}</CardDescription>
-                    </Card>
-                ))}
-            </WrapperCards>
+            <CardsList cards={cards} minMobileHeight={370} />
         </Section>
     )
 };
@@ -109,104 +86,6 @@ const Title = styled.h2`
         line-height: ${GRID(6.75)};
         margin: 0 0 ${GRID(14)};
     }
-`
-
-const SwiperMobile = styled(Swiper)`
-    padding: 0 0 ${GRID(5.5)};
-    max-width: ${GRID(42.875)};
-
-    @media only screen and (min-width: 1024px) {
-        display: none;
-    }
-
-    .swiper-wrapper {
-        > div {
-            min-height: ${GRID(45)};
-            border: ${GRID(0.125)} solid #E9E9E9;
-            padding: ${GRID(4)};
-            max-width: 100%;
-            border-radius: ${GRID(3)};
-        }
-    }
-
-    .swiper-button-prev {
-        width: ${GRID(4)};
-        height: ${GRID(4.125)};
-
-        top: unset;
-        left: unset;
-        right: ${GRID(6)};
-        bottom: 0;
-
-        &::after {
-            content: url('./icons/arrow-left.svg');
-            line-height: ${GRID(4)};
-        }
-    }
-
-    .swiper-button-next {
-        width: ${GRID(4)};
-        height: ${GRID(4.125)};
-
-        top: unset;
-        left: unset;
-        right: 0;
-        bottom: 0;
-
-        &::after {
-            content: url('./icons/arrow-right.svg');
-            line-height: ${GRID(4)};
-        }
-    }
-`
-
-const WrapperCards = styled.div`
-    display: none;
-    
-    @media only screen and (min-width: 1024px) {
-        gap: ${GRID(3)};
-        display: flex;
-        flex-wrap: wrap;
-        justify-content: space-between;
-    }
-`
-
-const Card = styled.div`
-    border: ${GRID(0.125)} solid #E9E9E9;
-    padding: ${GRID(4)};
-    max-width: ${GRID(42.875)};
-    border-radius: ${GRID(3)};
-    
-    
-    @media only screen and (min-width: 1024px) {
-        max-width: calc(33% - ${GRID(1.75)});
-
-        &:hover {
-            box-shadow: 0 ${GRID(0.5)} ${GRID(1)} rgba(0, 0, 0, 0.08);
-        }
-    }
-`
-
-const CardImg = styled.div`
-    width: ${GRID(12)};
-    height: ${GRID(12)};
-    margin: 0 0 ${GRID(3)};
-    background-size: cover;
-`
-
-const CardTitle = styled.h3`
-    color: #223154;
-    margin: 0 0 ${GRID(2.5)};
-    font-size: ${GRID(2.75)};
-    font-weight: 700;
-    line-height: ${GRID(4.125)};
-`
-
-const CardDescription = styled.p`
-    color: #8D95A7;
-    font-size: ${GRID(2)};
-    font-weight: 500;
-    line-height: ${GRID(3)};
 `
 
 export default Benefits;
