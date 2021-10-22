7a6758120b6737e58dd3ffc856fbb4416115cb84
Diego Roso da Silva <diego.silva@dito.com.br>
2021-10-22 10:43:54 -0300

    create screening section

---
+++ sections/Screening/Screening.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { COLORS, GRID } from "@ditointernet/uai-foundation";
+import styled from "styled-components";
+
+const Screening = () => {
+    return (
+        <h2>Screening</h2>
+    );
+}
+
+export default Screening;
