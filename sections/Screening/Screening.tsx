cfb5cbcdf7cebc706b45827b5c15e89562ca7b3c
Diego Roso da Silva <diego.silva@dito.com.br>
2021-10-22 15:04:22 -0300

    desktop view on screening section

--- sections/Screening/Screening.tsx
+++ sections/Screening/Screening.tsx
@@ -1,20 +1,30 @@
 import React from "react";
-import { COLORS, GRID } from "@ditointernet/uai-foundation";
 import styled from "styled-components";
+import { COLORS, GRID } from "@ditointernet/uai-foundation";
 
 const Screening = () => {
     return (
         <Section id="processo-seletivo">
-            <Title>Nosso processo seletivo</Title>
-            <Description>Entrevista inicial, entrevista técnica, desafio, entrevista de cultura, proposta e admissão. Saiba mais sobre as etapas necessárias para entrar para o nosso <b>time de FERAs.</b></Description>
-            <SubTitle number="01">Conversa inicial</SubTitle>
-            <Description>Depois do nosso primeiro contato, seja por cadastro, LinkedIn ou outra via, partimos para a nossa entrevista inicial ou vamos direto para a entrevista técnica.<br /><br />Essa é a oportunidade para você mostrar seus conhecimentos e suas competências, contar sobre sua trajetória profissional e sobre o que você busca para sua carreira, especialmente, aqui na Dito.</Description>
-            <SubTitle number="02">Apresentação do desafio</SubTitle>
-            <Description>Pode ser que a gente peça para você realizar um desafio – uma forma de entender melhor como você desenvolve seu trabalho.</Description>
-            <SubTitle number="03">Entrevista de cultura</SubTitle>
-            <Description>Correndo tudo bem, avançamos para a entrevista de cultura. Durante essa conversa, vamos identificando se o nosso jeito de trabalhar e os nossos valores fazem sentido para você.<br /><br />E, claro, se essa parceria tem tudo para ser especial, para as duas partes.</Description>
-            <SubTitle number="04">Deu match!</SubTitle>
-            <Description>Deu match! Agora falta pouco. Fazemos uma proposta para você e, se entrarmos em um acordo, chegamos no grande momento: a admissão.</Description>
+            <Fragment maxWidth={795}>
+                <Title>Nosso processo seletivo</Title>
+                <Description>Entrevista inicial, entrevista técnica, desafio, entrevista de cultura, proposta e admissão. Saiba mais sobre as etapas necessárias para entrar para o nosso <b>time de FERAs.</b></Description>
+            </Fragment>
+            <Fragment className="phases" maxWidth={816}>
+                <SubTitle number="01">Conversa inicial</SubTitle>
+                <Description>Depois do nosso primeiro contato, seja por cadastro, LinkedIn ou outra via, partimos para a nossa entrevista inicial ou vamos direto para a entrevista técnica.<br /><br />Essa é a oportunidade para você mostrar seus conhecimentos e suas competências, contar sobre sua trajetória profissional e sobre o que você busca para sua carreira, especialmente, aqui na Dito.</Description>
+            </Fragment>
+            <Fragment className="phases" maxWidth={639}>
+                <SubTitle number="02">Apresentação do desafio</SubTitle>
+                <Description>Pode ser que a gente peça para você realizar um desafio – uma forma de entender melhor como você desenvolve seu trabalho.</Description>
+            </Fragment>
+            <Fragment className="phases" maxWidth={709}>
+                <SubTitle number="03">Entrevista de cultura</SubTitle>
+                <Description>Correndo tudo bem, avançamos para a entrevista de cultura. Durante essa conversa, vamos identificando se o nosso jeito de trabalhar e os nossos valores fazem sentido para você.<br /><br />E, claro, se essa parceria tem tudo para ser especial, para as duas partes.</Description>
+            </Fragment>
+            <Fragment className="phases" maxWidth={770}>
+                <SubTitle number="04">Deu match!</SubTitle>
+                <Description>Deu match! Agora falta pouco. Fazemos uma proposta para você e, se entrarmos em um acordo, chegamos no grande momento: a admissão.</Description>
+            </Fragment>
             <Description><b>Ah, importante:</b> independentemente do resultado, sempre damos retorno – e o mais rápido possível. Essa é mais uma forma de mostrarmos que, de fato, nos importamos com as pessoas e com a experiência delas com a gente.</Description>
         </Section>
     );
@@ -23,6 +33,57 @@ const Screening = () => {
 const Section = styled.section`
     margin: 0 0 96px;
     padding: 96px ${GRID(2)} 0;
+    max-width: ${GRID(180)};
+
+    @media only screen and (min-width: 1024px) {
+        margin: 0 auto;
+        display: flex;
+        padding: ${GRID(20)} ${GRID(9)} ${GRID(15)};
+        flex-direction: column;
+
+        > div {
+            &:first-child {
+                p {
+                    padding: 0;
+                }
+            }
+            &:nth-child(2) {
+                margin-left: 66px;
+            }
+            &:nth-child(3) {
+                margin-right: 158px;
+            }
+            &:nth-child(4) {
+                margin-left: 26px;
+            }
+            &:nth-child(5) {
+                margin-right: 27px;
+            }
+        }
+
+        .phases {
+            &:nth-child(odd) {
+                align-self: flex-end;
+            }
+        }
+    
+        > p {
+            margin: 0 auto;
+            padding: 0;
+            max-width: 1043px;
+            text-align: center;
+        }
+    }
+
+`
+
+const Fragment = styled.div<{ maxWidth: number }>`
+    margin: 0 0 88px;
+    max-width: ${({ maxWidth }) => GRID(maxWidth / 8)};
+
+    @media only screen and (min-width: 1024px) {
+        margin: 0 0 130px;
+    }
 `
 
 const Title = styled.h2`
@@ -31,6 +92,12 @@ const Title = styled.h2`
     font-size: 22px;
     font-weight: 700;
     line-height: 33px;
+
+    @media only screen and (min-width: 1024px) {
+        margin: 0 0 ${GRID(3)};
+        font-size: 36px;
+        line-height: 54px;
+    }
 `
 
 const SubTitle = styled.h3<{ number: string }>`
@@ -42,6 +109,12 @@ const SubTitle = styled.h3<{ number: string }>`
     font-weight: 700;
     line-height: 33px;
 
+    @media only screen and (min-width: 1024px) {
+        padding: 52px 0 0 29px;
+        font-size: 28px;
+        line-height: 42px;
+    }
+
     ::before {
         top: 0;
         left: 0;
@@ -52,12 +125,17 @@ const SubTitle = styled.h3<{ number: string }>`
         font-size: 36px;
         font-weight: 700;
         line-height: 54px;
+
+        @media only screen and (min-width: 1024px) {
+            font-size: 64px;
+            line-height: 96px;
+        }
     }
 `
 
 const Description = styled.p`
     color: ${COLORS.GRAY_4};
-    margin: 0 0 88px;
+    margin: 0;
     font-size: 16px;
     font-weight: 500;
     line-height: 24px;
@@ -65,6 +143,10 @@ const Description = styled.p`
     b {
         color: ${COLORS.NAVY_DARK};
         font-weight: 700;
+    }
+
+    @media only screen and (min-width: 1024px) {
+        padding: 0 0 0 40px;
     }
 `
 
