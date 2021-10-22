c7abaacba6d8b69c17d85384c8dd5a4a66112ffd
Diego Roso da Silva <diego.silva@dito.com.br>
2021-10-22 17:15:22 -0300

    include background on screening section

--- sections/Screening/Screening.tsx
+++ sections/Screening/Screening.tsx
@@ -5,32 +5,56 @@ import { COLORS, GRID } from "@ditointernet/uai-foundation";
 const Screening = () => {
     return (
         <Section id="processo-seletivo">
-            <Fragment maxWidth={795}>
-                <Title>Nosso processo seletivo</Title>
-                <Description>Entrevista inicial, entrevista técnica, desafio, entrevista de cultura, proposta e admissão. Saiba mais sobre as etapas necessárias para entrar para o nosso <b>time de FERAs.</b></Description>
-            </Fragment>
-            <Fragment className="phases" maxWidth={816}>
-                <SubTitle number="01">Conversa inicial</SubTitle>
-                <Description>Depois do nosso primeiro contato, seja por cadastro, LinkedIn ou outra via, partimos para a nossa entrevista inicial ou vamos direto para a entrevista técnica.<br /><br />Essa é a oportunidade para você mostrar seus conhecimentos e suas competências, contar sobre sua trajetória profissional e sobre o que você busca para sua carreira, especialmente, aqui na Dito.</Description>
-            </Fragment>
-            <Fragment className="phases" maxWidth={639}>
-                <SubTitle number="02">Apresentação do desafio</SubTitle>
-                <Description>Pode ser que a gente peça para você realizar um desafio – uma forma de entender melhor como você desenvolve seu trabalho.</Description>
-            </Fragment>
-            <Fragment className="phases" maxWidth={709}>
-                <SubTitle number="03">Entrevista de cultura</SubTitle>
-                <Description>Correndo tudo bem, avançamos para a entrevista de cultura. Durante essa conversa, vamos identificando se o nosso jeito de trabalhar e os nossos valores fazem sentido para você.<br /><br />E, claro, se essa parceria tem tudo para ser especial, para as duas partes.</Description>
-            </Fragment>
-            <Fragment className="phases" maxWidth={770}>
-                <SubTitle number="04">Deu match!</SubTitle>
-                <Description>Deu match! Agora falta pouco. Fazemos uma proposta para você e, se entrarmos em um acordo, chegamos no grande momento: a admissão.</Description>
-            </Fragment>
-            <Description><b>Ah, importante:</b> independentemente do resultado, sempre damos retorno – e o mais rápido possível. Essa é mais uma forma de mostrarmos que, de fato, nos importamos com as pessoas e com a experiência delas com a gente.</Description>
+            <SectionWrapper>
+                <Fragment maxWidth={795}>
+                    <Title>Nosso processo seletivo</Title>
+                    <Description>Entrevista inicial, entrevista técnica, desafio, entrevista de cultura, proposta e admissão. Saiba mais sobre as etapas necessárias para entrar para o nosso <b>time de FERAs.</b></Description>
+                </Fragment>
+                <Fragment className="phases" maxWidth={816}>
+                    <SubTitle number="01">Conversa inicial</SubTitle>
+                    <Description>Depois do nosso primeiro contato, seja por cadastro, LinkedIn ou outra via, partimos para a nossa entrevista inicial ou vamos direto para a entrevista técnica.<br /><br />Essa é a oportunidade para você mostrar seus conhecimentos e suas competências, contar sobre sua trajetória profissional e sobre o que você busca para sua carreira, especialmente, aqui na Dito.</Description>
+                </Fragment>
+                <Fragment className="phases" maxWidth={639}>
+                    <SubTitle number="02">Apresentação do desafio</SubTitle>
+                    <Description>Pode ser que a gente peça para você realizar um desafio – uma forma de entender melhor como você desenvolve seu trabalho.</Description>
+                </Fragment>
+                <Fragment className="phases" maxWidth={709}>
+                    <SubTitle number="03">Entrevista de cultura</SubTitle>
+                    <Description>Correndo tudo bem, avançamos para a entrevista de cultura. Durante essa conversa, vamos identificando se o nosso jeito de trabalhar e os nossos valores fazem sentido para você.<br /><br />E, claro, se essa parceria tem tudo para ser especial, para as duas partes.</Description>
+                </Fragment>
+                <Fragment className="phases" maxWidth={770}>
+                    <SubTitle number="04">Deu match!</SubTitle>
+                    <Description>Deu match! Agora falta pouco. Fazemos uma proposta para você e, se entrarmos em um acordo, chegamos no grande momento: a admissão.</Description>
+                </Fragment>
+                <Description><b>Ah, importante:</b> independentemente do resultado, sempre damos retorno – e o mais rápido possível. Essa é mais uma forma de mostrarmos que, de fato, nos importamos com as pessoas e com a experiência delas com a gente.</Description>
+            </SectionWrapper>
         </Section>
     );
 }
 
 const Section = styled.section`
+    position: relative;
+
+    @media only screen and (min-width: 1024px) {
+        &::before {
+            top: 630px;
+            left: 0;
+            z-index: -1;
+            content: url('./images/screening-left.svg');
+            position: absolute;
+        }
+
+        &::after {
+            top: 310px;
+            right: 0;
+            z-index: -1;
+            content: url('./images/screening-right.svg');
+            position: absolute;
+        }
+    }
+`
+
+const SectionWrapper = styled.div`
     margin: 0 0 96px;
     padding: 96px ${GRID(2)} 0;
     max-width: ${GRID(180)};
@@ -40,7 +64,7 @@ const Section = styled.section`
         display: flex;
         padding: ${GRID(20)} ${GRID(9)} ${GRID(15)};
         flex-direction: column;
-
+        
         > div {
             &:first-child {
                 p {
@@ -66,7 +90,7 @@ const Section = styled.section`
                 align-self: flex-end;
             }
         }
-    
+
         > p {
             margin: 0 auto;
             padding: 0;
