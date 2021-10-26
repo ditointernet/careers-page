44d5eab7c2ba47b9dab2029b8d3c44f99eb52d8f
Diego Roso da Silva <diego.silva@dito.com.br>
2021-10-26 16:43:32 -0300

    change element name

--- sections/Screening/Screening.tsx
+++ sections/Screening/Screening.tsx
@@ -6,26 +6,26 @@ const Screening = () => {
     return (
         <Section id="processo-seletivo">
             <SectionWrapper>
-                <Fragment maxWidth={795}>
+                <Content maxWidth={795}>
                     <Title>Nosso processo seletivo</Title>
                     <Description>Entrevista inicial, entrevista técnica, desafio, entrevista de cultura, proposta e admissão. Saiba mais sobre as etapas necessárias para entrar para o nosso <b>time de FERAs.</b></Description>
-                </Fragment>
-                <Fragment className="phases" maxWidth={816}>
+                </Content>
+                <Content className="phases" maxWidth={816}>
                     <SubTitle number="01">Conversa inicial</SubTitle>
                     <Description>Depois do nosso primeiro contato, seja por cadastro, LinkedIn ou outra via, partimos para a nossa entrevista inicial ou vamos direto para a entrevista técnica.<br /><br />Essa é a oportunidade para você mostrar seus conhecimentos e suas competências, contar sobre sua trajetória profissional e sobre o que você busca para sua carreira, especialmente, aqui na Dito.</Description>
-                </Fragment>
-                <Fragment className="phases" maxWidth={639}>
+                </Content>
+                <Content className="phases" maxWidth={639}>
                     <SubTitle number="02">Apresentação do desafio</SubTitle>
                     <Description>Pode ser que a gente peça para você realizar um desafio – uma forma de entender melhor como você desenvolve seu trabalho.</Description>
-                </Fragment>
-                <Fragment className="phases" maxWidth={709}>
+                </Content>
+                <Content className="phases" maxWidth={709}>
                     <SubTitle number="03">Entrevista de cultura</SubTitle>
                     <Description>Correndo tudo bem, avançamos para a entrevista de cultura. Durante essa conversa, vamos identificando se o nosso jeito de trabalhar e os nossos valores fazem sentido para você.<br /><br />E, claro, se essa parceria tem tudo para ser especial, para as duas partes.</Description>
-                </Fragment>
-                <Fragment className="phases" maxWidth={770}>
+                </Content>
+                <Content className="phases" maxWidth={770}>
                     <SubTitle number="04">Deu match!</SubTitle>
                     <Description>Deu match! Agora falta pouco. Fazemos uma proposta para você e, se entrarmos em um acordo, chegamos no grande momento: a admissão.</Description>
-                </Fragment>
+                </Content>
                 <Description><b>Ah, importante:</b> independentemente do resultado, sempre damos retorno – e o mais rápido possível. Essa é mais uma forma de mostrarmos que, de fato, nos importamos com as pessoas e com a experiência delas com a gente.</Description>
             </SectionWrapper>
         </Section>
@@ -101,7 +101,7 @@ const SectionWrapper = styled.div`
 
 `
 
-const Fragment = styled.div<{ maxWidth: number }>`
+const Content = styled.div<{ maxWidth: number }>`
     margin: 0 0 ${GRID(11)};
     max-width: ${({ maxWidth }) => GRID(maxWidth / 8)};
 
