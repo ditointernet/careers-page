ef062930fbacd48bbbe3a50d8e839c2dc1f9482e
Diego Roso da Silva <diego.silva@dito.com.br>
2021-10-27 17:33:35 -0300

    add lines details

--- sections/Tech/Tech.tsx
+++ sections/Tech/Tech.tsx
@@ -4,40 +4,46 @@ import { COLORS, GRID } from '@ditointernet/uai-foundation';
 
 const Tech = () => (
     <Section>
-        <WrapperSection>
+        <SectionWrapper>
             <Title>Já somos uma grande Martech* e não paramos de crescer.</Title>
-            <Description marginBottomMobile={71} marginBottomDesktop={48} maxWidth={694} className="arrowRight">A plataforma Dito foi toda desenvolvida internamente, com <b>~/tecnologia própria/~</b>, e impacta <b>~/milhões de consumidores/~</b> por mês, de forma inteligente e multicanal.</Description>
             <Container>
-                <WrapperBoxes>
-                    <Description marginBottomMobile={25} marginBottomDesktop={36}>Hoje usamos tecnologias como:</Description>
-                    <Box>
-                        <BoxContent>
-                            <BoxItem>Golang</BoxItem>
-                            <BoxItem className="hyphen">Kubernetes</BoxItem>
-                            <BoxItem className="hyphen">Elasticsearch (cluster de 210 bilhoes de documentos)</BoxItem>
-                            <BoxItem className="hyphen">Kinesis</BoxItem>
-                            <BoxItem>AWS e GCP como cloud</BoxItem>
-                        </BoxContent>
-                    </Box>
-                    <Description marginBottomMobile={25} marginBottomDesktop={36}>Além de:</Description>
-                    <Box>
-                        <BoxContent>
-                            <BoxItem>React</BoxItem>
-                            <BoxItem className="hyphen">Design System próprio (DitoUAI)</BoxItem>
-                            <BoxItem className="hyphen">Typescript</BoxItem>
-                            <BoxItem className="hyphen">Styled-components</BoxItem>
-                            <BoxItem>Redux</BoxItem>
-                        </BoxContent>
-                    </Box>
-                </WrapperBoxes>
-                <WrapperNumbers>
-                    <Description marginBottomMobile={24} marginBottomDesktop={20}>Alguns dos nossos números:</Description>
-                    <Description marginBottomMobile={32} marginBottomDesktop={40} className="contrast blue" maxWidth={750}>São <b>676 rotinas periódicas</b> e mais de <b>180 microsserviços</b>, totalizando <b>284 instâncias</b>, rodando em <b>58 máquinas</b>.</Description>
-                    <Description marginBottomMobile={24} marginBottomDesktop={24} className="blue" maxWidth={622}>Ah, e se você ainda não domina a linguagem <b>Go (Golang)</b>, mas tem muita vontade, não se preocupe porque a gente tem todo o prazer em ensinar. A maior parte do nosso time aprendeu aqui mesmo.</Description>
-                    <Description marginBottomMobile={36} maxWidth={622}>Por aqui, a gente valoriza tanto o aprendizado que ele faz parte da nossa rotina. Além do nosso code review, em que todo o time aproveita para tirar dúvidas, temos o hackathon, um momento de troca livre de experiências e conhecimentos para propormos soluções para melhorar processos, códigos etc.</Description>
-                </WrapperNumbers>
+                <Description marginBottomMobile={71} marginBottomDesktop={48} maxWidth={694} className="arrowRight">A plataforma Dito foi toda desenvolvida internamente, com <b>~/tecnologia própria/~</b>, e impacta <b>~/milhões de consumidores/~</b> por mês, de forma inteligente e multicanal.</Description>
+                <MainWrapper>
+                    <BoxesWrapper>
+                        <Description marginBottomMobile={25} marginBottomDesktop={36}>Hoje usamos tecnologias como:</Description>
+                        <BoxShadow>
+                            <Box>
+                                <BoxContent>
+                                    <BoxItem>Golang</BoxItem>
+                                    <BoxItem className="hyphen">Kubernetes</BoxItem>
+                                    <BoxItem className="hyphen">Elasticsearch (cluster de 210 bilhoes de documentos)</BoxItem>
+                                    <BoxItem className="hyphen">Kinesis</BoxItem>
+                                    <BoxItem>AWS e GCP como cloud</BoxItem>
+                                </BoxContent>
+                            </Box>
+                        </BoxShadow>
+                        <Description marginBottomMobile={25} marginBottomDesktop={36}>Além de:</Description>
+                        <BoxShadow>
+                            <Box>
+                                <BoxContent>
+                                    <BoxItem>React</BoxItem>
+                                    <BoxItem className="hyphen">Design System próprio (DitoUAI)</BoxItem>
+                                    <BoxItem className="hyphen">Typescript</BoxItem>
+                                    <BoxItem className="hyphen">Styled-components</BoxItem>
+                                    <BoxItem>Redux</BoxItem>
+                                </BoxContent>
+                            </Box>
+                        </BoxShadow>
+                    </BoxesWrapper>
+                    <NumbersWrapper>
+                        <Description marginBottomMobile={24} marginBottomDesktop={20} className="indicator">Alguns dos nossos números:</Description>
+                        <Description marginBottomMobile={32} marginBottomDesktop={40} className="contrast blue" maxWidth={750}>São <b>676 rotinas periódicas</b> e mais de <b>180 microsserviços</b>, totalizando <b>284 instâncias</b>, rodando em <b>58 máquinas</b>.</Description>
+                        <Description marginBottomMobile={24} marginBottomDesktop={24} className="blue" maxWidth={622}>Ah, e se você ainda não domina a linguagem <b>Go (Golang)</b>, mas tem muita vontade, não se preocupe porque a gente tem todo o prazer em ensinar. A maior parte do nosso time aprendeu aqui mesmo.</Description>
+                        <Description marginBottomMobile={36} maxWidth={622}>Por aqui, a gente valoriza tanto o aprendizado que ele faz parte da nossa rotina. Além do nosso code review, em que todo o time aproveita para tirar dúvidas, temos o hackathon, um momento de troca livre de experiências e conhecimentos para propormos soluções para melhorar processos, códigos etc.</Description>
+                    </NumbersWrapper>
+                </MainWrapper>
             </Container>
-        </WrapperSection>
+        </SectionWrapper>
     </Section>
 );
 
@@ -70,7 +76,7 @@ const Section = styled.section`
     }
 `
 
-const WrapperSection = styled.div`
+const SectionWrapper = styled.div`
     margin: 0 auto;
     padding: 40px 16px;
     max-width: 1440px;
@@ -100,18 +106,14 @@ const Title = styled.h2`
 `
 
 const Container = styled.div`
-    display: flex;
     padding: 0 0 0 22px;
     position: relative;
     border-left: 1px solid #6A7486;
     border-image: linear-gradient(to bottom, #121A2D 22px, #6A7486 0,#6A7486 calc(100% - 22px),#121A2D 0);
-    flex-direction: column;
     border-image-slice: 1;
     
     @media only screen and (min-width: 1024px) {
-        gap: 108px;
         padding: 0 0 0 24px;
-        flex-direction: row;
     }
 
     &::before {
@@ -123,7 +125,6 @@ const Container = styled.div`
         content: '';
         position: absolute;
         border-radius: 100%;
-        background-color: #121A2D;
     }
 
     &::after {
@@ -135,11 +136,20 @@ const Container = styled.div`
         content: '';
         position: absolute;
         border-radius: 100%;
-        background-color: #121A2D;
     }
 `
 
-const WrapperBoxes = styled.div`
+const MainWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+
+    @media only screen and (min-width: 1024px) {
+        gap: 108px;
+        flex-direction: row;
+    }
+`
+
+const BoxesWrapper = styled.div`
     margin: 0 0 19px 0;
     display: flex;
     flex-direction: column;
@@ -149,7 +159,7 @@ const WrapperBoxes = styled.div`
     }
 `
 
-const WrapperNumbers = styled.div`
+const NumbersWrapper = styled.div`
     display: flex;
     flex-direction: column;
 
@@ -205,24 +215,69 @@ const Description = styled.p<{
             font-weight: 700;
         }
     }
+    
+    &.indicator {
+        @media only screen and (min-width: 1024px) {
+            position: inherit;
+            
+            &::before {
+                top: 187px;
+                left: 0px;
+                width: 500px;
+                height: 1px;
+                content: '';
+                position: absolute;
+                border-top: 1px solid #6A7486;
+            }
+
+            &::after {
+                top: 183px;
+                left: 504px;
+                width: 8px;
+                height: 8px;
+                border: 1px solid #6A7486;
+                content: '';
+                position: absolute;
+                border-radius: 100%;
+                background-color: #121A2D;
+            }
+        }
+    }
 `
-
-const Box = styled.div`
-    border: 1px solid #2E384E;
-    padding: 20px 16px 20px 26px;
-    position: relative;
-    background: #0D1425;
-    box-sizing: border-box;
-    box-shadow: 0px 7px 20px -2px rgba(0, 0, 0, 0.8);
-    border-radius: 16px;
+const BoxShadow = styled.div`
     width: 100%;
-    max-width: 356px;
     margin: 0 0 36px 0;
-
+    z-index: 1;
+    position: relative;
+    max-width: 356px;
+    
     @media only screen and (min-width: 1024px) {
         width: 356px;
         margin: 0 0 48px 34px;
     }
+
+    &::before {
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        content: '';
+        position: absolute;
+        border-radius: 100%;
+        box-shadow: 0px 0px 61px 20px rgba(3, 255, 210, 0.2);
+    }
+
+`
+
+const Box = styled.div`
+    width: 100%;
+    border: 1px solid #2E384E;
+    padding: 20px 16px 20px 26px;
+    position: relative;
+    box-sizing: border-box;
+    box-shadow: 0px 7px 20px -2px rgba(0, 0, 0, 0.8);
+    border-radius: 16px;
+    background-color: #0D1425;
 
     &::before {
         top: calc(50% - 24px);
