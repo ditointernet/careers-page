7c13f4099d88728bc9c2d2febd87fb642d465b8d
Diego Roso da Silva <diego.silva@dito.com.br>
2021-10-28 11:33:00 -0300

    background of area section

--- sections/Areas/Areas.tsx
+++ sections/Areas/Areas.tsx
@@ -5,51 +5,64 @@ import ButtonSecondary from "@/components/ButtonSecondary";
 
 const Areas = () => (
     <Section>
-        <Title>Nossas áreas</Title>
-        <Description
-            marginBottomMobile={60}
-            marginBottomDesktop={109}
-            style={{ maxWidth: 580 }}
-        >
-            Hoje o nosso time de FERAs está dividido em <b>3 grandes</b> áreas que, juntas, são responsáveis por entregar um <b>produto de qualidade</b> e que nossos clientes amam. Dá só uma olhada:
-        </Description>
-        <AreaWrapper>
-            <Area maxWidth={1051} margin="0 0 0 79px">
-                <AreaImg image="./images/area-tecnologia.svg" />
-                <AreaContent>
-                    <AreaTitle>Tecnologia</AreaTitle>
-                    <Description marginBottomMobile={32} marginBottomDesktop={36}>É a nossa maior área, responsável por <b>dinamizar a plataforma</b> através das linguagens de programação (front e back-end) e por cuidar da <b>arquitetura e da infraestrutura</b> dos nossos produtos. Além disso, se encarregam de <b>manter</b> e <b>melhorar</b> os produtos, mapear e garantir o desenvolvimento de <b>novas funcionalidades</b> e assegurar uma <b>boa experiência</b> para as pessoas usuárias.</Description>
-                    <ButtonSecondary>Ver vagas de Tecnologia</ButtonSecondary>
-                </AreaContent>
-            </Area>
-            <Area maxWidth={957} margin="0 53px 0 0">
-                <AreaImg image="./images/area-comercial.svg" />
-                <AreaContent>
-                    <AreaTitle>Comercial</AreaTitle>
-                    <Description marginBottomMobile={32} marginBottomDesktop={36}>O Comercial é a ponte entre nosso produto e o mundo, responsável por promover ações que visam <b>difundir nossa marca</b> e nos conectar com <b>potenciais clientes</b>. Essa área é composta por <b>Vendas</b>, <b>Customer Success (CS)</b>, <b>Marketing</b> e <b>Suporte ao Cliente</b>.</Description>
-                    <ButtonSecondary>Ver vagas de Comercial</ButtonSecondary>
-                </AreaContent>
-            </Area>
-            <Area maxWidth={972} margin="0 0 0 127px">
-                <AreaImg image="./images/area-gente-gestao.svg" />
-                <AreaContent>
-                    <AreaTitle>Gente & Gestão</AreaTitle>
-                    <Description marginBottomMobile={32} marginBottomDesktop={36}>A área reúne <b>Pessoas</b>, <b>Planejamento & Gestão</b> e <b>Financeiro</b>. Os times são responsáveis por trazer mais FERAs, cuidam da gestão do clima e do desenvolvimento das pessoas, definem os caminhos a serem tomados e administram os recursos financeiros da nossa empresa.</Description>
-                    <ButtonSecondary>Ver vagas de Gente & Gestão</ButtonSecondary>
-                </AreaContent>
-            </Area>
-        </AreaWrapper>
+        <WrapperSection>
+            <Title>Nossas áreas</Title>
+            <Description
+                marginBottomMobile={60}
+                marginBottomDesktop={109}
+                style={{ maxWidth: 580 }}
+            >
+                Hoje o nosso time de FERAs está dividido em <b>3 grandes</b> áreas que, juntas, são responsáveis por entregar um <b>produto de qualidade</b> e que nossos clientes amam. Dá só uma olhada:
+            </Description>
+            <AreaWrapper>
+                <Area maxWidth={1051} margin="0 0 0 79px">
+                    <AreaImg image="./images/area-tecnologia.svg" />
+                    <AreaContent>
+                        <AreaTitle>Tecnologia</AreaTitle>
+                        <Description marginBottomMobile={32} marginBottomDesktop={36}>É a nossa maior área, responsável por <b>dinamizar a plataforma</b> através das linguagens de programação (front e back-end) e por cuidar da <b>arquitetura e da infraestrutura</b> dos nossos produtos. Além disso, se encarregam de <b>manter</b> e <b>melhorar</b> os produtos, mapear e garantir o desenvolvimento de <b>novas funcionalidades</b> e assegurar uma <b>boa experiência</b> para as pessoas usuárias.</Description>
+                        <ButtonSecondary>Ver vagas de Tecnologia</ButtonSecondary>
+                    </AreaContent>
+                </Area>
+                <Area maxWidth={957} margin="0 53px 0 0">
+                    <AreaImg image="./images/area-comercial.svg" />
+                    <AreaContent>
+                        <AreaTitle>Comercial</AreaTitle>
+                        <Description marginBottomMobile={32} marginBottomDesktop={36}>O Comercial é a ponte entre nosso produto e o mundo, responsável por promover ações que visam <b>difundir nossa marca</b> e nos conectar com <b>potenciais clientes</b>. Essa área é composta por <b>Vendas</b>, <b>Customer Success (CS)</b>, <b>Marketing</b> e <b>Suporte ao Cliente</b>.</Description>
+                        <ButtonSecondary>Ver vagas de Comercial</ButtonSecondary>
+                    </AreaContent>
+                </Area>
+                <Area maxWidth={972} margin="0 0 0 127px">
+                    <AreaImg image="./images/area-gente-gestao.svg" />
+                    <AreaContent>
+                        <AreaTitle>Gente & Gestão</AreaTitle>
+                        <Description marginBottomMobile={32} marginBottomDesktop={36}>A área reúne <b>Pessoas</b>, <b>Planejamento & Gestão</b> e <b>Financeiro</b>. Os times são responsáveis por trazer mais FERAs, cuidam da gestão do clima e do desenvolvimento das pessoas, definem os caminhos a serem tomados e administram os recursos financeiros da nossa empresa.</Description>
+                        <ButtonSecondary>Ver vagas de Gente & Gestão</ButtonSecondary>
+                    </AreaContent>
+                </Area>
+            </AreaWrapper>
+        </WrapperSection>
     </Section>
 )
 
 const Section = styled.section`
-    margin: 0 auto;
-    padding: 0 ${GRID(2)};
-    max-width: ${GRID(180)};
+    background-size: cover;
+    background-image: url("./images/areas-mb-bg.svg");
+    background-repeat: no-repeat;
+    background-position: inherit;
     
     @media only screen and (min-width: 1024px) {
+        background-image: url("./images/areas-bg.svg");
+    }
+`
+
+const WrapperSection = styled.div`
+    margin: 0 auto;
+    padding: ${GRID(12.75)} ${GRID(2)} ${GRID(22.5)} ${GRID(2)};
+    max-width: ${GRID(180)};
+
+    @media only screen and (min-width: 1024px) {
         margin: 0 auto;
-        padding: 0 ${GRID(9)};
+        padding: ${GRID(13.375)} ${GRID(9)} ${GRID(26.125)} ${GRID(9)};
     }
 `
 
