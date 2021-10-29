066a779c5d3882630c7d5ed1582ccdd5624ab368
Diego Roso da Silva <diego.silva@dito.com.br>
2021-10-29 18:05:57 -0300

    include GTM

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -69,6 +69,11 @@ class CustomDocument extends Document {
           />
         </Head>
         <body>
+          {/* Google Tag Manager (noscript) */}
+          <noscript dangerouslySetInnerHTML={{
+            __html: "<iframe src=\"https://www.googletagmanager.com/ns.html?id=GTM-52QG6W3\" height=\"0\" width=\"0\" style=\"display:none;visibility:hidden\"></iframe>"
+          }}></noscript>
+          {/* End Google Tag Manager (noscript) */}
           <Main />
           <NextScript />
         </body>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,19 @@
 import { NextPage } from "next";
 import Head from "next/head";
+import Script from "next/script"
 import * as Sections from "../sections";
 
 const Home: NextPage = () => (
   <main>
+    {/* Google Tag Manager */}
+    <Script id="google-analytics" strategy="afterInteractive">
+      {`(function(w,d,s,l,i){w[l] = w[l] || [];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-52QG6W3');`}
+    </Script>
+    {/* End Google Tag Manager */}
     <Head>
       <title>
         Carreiras Dito - Nossa paixão por tecnologia está revolucionando o
