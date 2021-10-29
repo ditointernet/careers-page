f344f9256e7232645161d434b13a8f27ea0eb4f9
Diego Roso da Silva <diego.silva@dito.com.br>
2021-10-29 09:49:12 -0300

    iniciatives section improvement

--- sections/Initiatives/Initiatives.tsx
+++ sections/Initiatives/Initiatives.tsx
@@ -15,19 +15,19 @@ const Initiatives = () => {
     const principalInitiatives: any = [
         {
             name: "AfroDito",
-            img: "./images/afro.jpg",
+            img: "./images/afro.png",
         },
         {
             name: "DitoPride",
-            img: "./images/pride.jpg",
+            img: "./images/pride.png",
         },
         {
             name: "DitoGirls",
-            img: "./images/girls.jpg",
+            img: "./images/girls.png",
         },
         {
             name: "DitoParents",
-            img: "./images/parents.jpg",
+            img: "./images/parents.png",
         }
     ]
 
@@ -60,49 +60,64 @@ const Initiatives = () => {
 
     return (
         <Section>
-            <Title>Nossas iniciativas</Title>
-            <Fragment>
-                <SubTitle>Comitês de Diversidade e Inclusão</SubTitle>
-                <Description>AfroDito, DitoPride, DitoGirls, DitoParents – que realizam eventos direcionados, debates e iniciativas de conscientização e inclusão de grupos minorizados.</Description>
-                <InitiativeWrapperDesktop>
-                    {principalInitiatives.map((initiative: any, index: number) => (
-                        <InitiativeSlide key={index}>
-                            <InitiativeImg image={initiative.img} />
-                            <InitiativeTitle>{initiative.name}</InitiativeTitle>
-                        </InitiativeSlide>
-                    ))}
-                </InitiativeWrapperDesktop>
-                <InitiativeSwiperMobile
-                    loop={true}
-                    navigation={true}
-                    slidesPerView={"auto"}
-                    slidesPerGroup={1}
-                    centeredSlides={true}
-                >
-                    {principalInitiatives.map((initiative: any, index: number) => (
-                        <SwiperSlide style={{ width: "50%" }} key={index}>
-                            <InitiativeSlide>
-                                <InitiativeImg className="initiativeImage" image={initiative.img} />
-                                <InitiativeTitle className="initiativeName">{initiative.name}</InitiativeTitle>
+            <SectionWrapper>
+                <Title>Nossas iniciativas</Title>
+                <Fragment>
+                    <SubTitle>Comitês de Diversidade e Inclusão</SubTitle>
+                    <Description>AfroDito, DitoPride, DitoGirls, DitoParents – que realizam eventos direcionados, debates e iniciativas de conscientização e inclusão de grupos minorizados.</Description>
+                    <InitiativeWrapperDesktop>
+                        {principalInitiatives.map((initiative: any, index: number) => (
+                            <InitiativeSlide key={index}>
+                                <InitiativeImg image={initiative.img} />
+                                <InitiativeTitle>{initiative.name}</InitiativeTitle>
                             </InitiativeSlide>
-                        </SwiperSlide>
-                    ))}
-                </InitiativeSwiperMobile>
-            </Fragment>
-            <Fragment>
-                <SubTitle>Dito Impacto</SubTitle>
-                <Description>Uma iniciativa criada para retribuir para a sociedade e multiplicar o sentimento de solidariedade. Além de ações de voluntariado, auxiliamos grupos ou instituições que lutam por causas de extrema importância com a ajuda do nosso time de FERAs.</Description>
-            </Fragment>
-            <SubTitle>Outras iniciativas</SubTitle>
-            <CardsList minMobileHeight={285} minDesktopHeight={285} cards={othersInitiatives} />
+                        ))}
+                    </InitiativeWrapperDesktop>
+                    <InitiativeSwiperMobile
+                        loop={true}
+                        navigation={true}
+                        slidesPerView={"auto"}
+                        slidesPerGroup={1}
+                        centeredSlides={true}
+                    >
+                        {principalInitiatives.map((initiative: any, index: number) => (
+                            <SwiperSlide style={{ width: "50%" }} key={index}>
+                                <InitiativeSlide>
+                                    <InitiativeImg className="initiativeImage" image={initiative.img} />
+                                    <InitiativeTitle className="initiativeName">{initiative.name}</InitiativeTitle>
+                                </InitiativeSlide>
+                            </SwiperSlide>
+                        ))}
+                    </InitiativeSwiperMobile>
+                </Fragment>
+                <Fragment>
+                    <SubTitle>Dito Impacto</SubTitle>
+                    <Description>Uma iniciativa criada para retribuir para a sociedade e multiplicar o sentimento de solidariedade. Além de ações de voluntariado, auxiliamos grupos ou instituições que lutam por causas de extrema importância com a ajuda do nosso time de FERAs.</Description>
+                </Fragment>
+                <SubTitle>Outras iniciativas</SubTitle>
+                <CardsList minMobileHeight={285} minDesktopHeight={285} cards={othersInitiatives} />
+            </SectionWrapper>
         </Section>
     )
 };
 
 const Section = styled.section`
-    padding: ${GRID(8)} ${GRID(2)} ${GRID(10)};
-    max-width: ${GRID(180)};
+    margin: ${GRID(1)} 0;
+    background-color: #F8F8F8;
+
+    @media only screen and (min-width: 1024px) {
+        margin: ${GRID(2)} 0;
+    }
+`
+
+const SectionWrapper = styled.section`
     margin: 0 auto;
+    padding: ${GRID(8)} ${GRID(2)};
+    max-width: ${GRID(180)};
+    
+    @media only screen and (min-width: 1024px) {
+        padding: ${GRID(10)} ${GRID(9)};
+    }
 `
 
 const Title = styled.h2`
