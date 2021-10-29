9b48a9c5857ee18d8030b1761f057601e2e4a821
Diego Roso da Silva <diego.silva@dito.com.br>
2021-10-29 10:23:46 -0300

    about section improvement

--- sections/About/About.tsx
+++ sections/About/About.tsx
@@ -3,49 +3,59 @@ import styled from "styled-components";
 import { COLORS, GRID } from "@ditointernet/uai-foundation";
 
 const About: NextPage = () => (
-  <Container>
-    <Subtitle>Sabemos onde queremos chegar:</Subtitle>
-    <Title>Ser a maior autoridade em CRM no Brasil com atuação global.</Title>
-    <TextWrap>
-      <Text>
-        A plataforma Dito nasceu em 2008 para ajudar{" "}
-        <strong>grandes marcas do varejo</strong> no relacionamento e
-        gerenciamento de clientes e, assim, não pararem de vender.
-      </Text>
-      <Text>
-        Com base em <strong>tecnologia e dados</strong>, criamos estratégias
-        centradas nas pessoas para oferecer produtos cada vez mais inovadores e
-        ajustados às necessidades do mercado.
-      </Text>
-      <Text>
-        Em 2020, influenciamos mais de{" "}
-        <strong>R$ 2.5 bilhões em receita de grandes marcas</strong> e estamos
-        trabalhando para atingir uma ARR (Receita Anual Recorrente) superior a{" "}
-        <strong>R$500 milhões</strong> até 2028.
-      </Text>
-      <Text>
-        Se você também sabe onde quer chegar, vai ser incrível ter você com a
-        gente.
-      </Text>
-    </TextWrap>
-    <FrameWrap>
-      <Frame
-        alt="imagem de um homem andando sob um gráfico"
-        src="/images/frame-0.png"
-      />
-    </FrameWrap>
-  </Container>
+  <Section>
+    <SectionWrapper>
+      <Subtitle>Sabemos onde queremos chegar:</Subtitle>
+      <Title>Ser a maior autoridade em CRM no Brasil com atuação global.</Title>
+      <TextWrap>
+        <Text>
+          A plataforma Dito nasceu em 2008 para ajudar{" "}
+          <strong>grandes marcas do varejo</strong> no relacionamento e
+          gerenciamento de clientes e, assim, não pararem de vender.
+        </Text>
+        <Text>
+          Com base em <strong>tecnologia e dados</strong>, criamos estratégias
+          centradas nas pessoas para oferecer produtos cada vez mais inovadores e
+          ajustados às necessidades do mercado.
+        </Text>
+        <Text>
+          Em 2020, influenciamos mais de{" "}
+          <strong>R$ 2.5 bilhões em receita de grandes marcas</strong> e estamos
+          trabalhando para atingir uma ARR (Receita Anual Recorrente) superior a{" "}
+          <strong>R$500 milhões</strong> até 2028.
+        </Text>
+        <Text>
+          Se você também sabe onde quer chegar, vai ser incrível ter você com a
+          gente.
+        </Text>
+      </TextWrap>
+      <FrameWrap>
+        <Frame
+          alt="imagem de um homem andando sob um gráfico"
+          src="/images/frame-0.png"
+        />
+      </FrameWrap>
+    </SectionWrapper>
+  </Section>
 );
 
-const Container = styled.section`
-  margin: ${GRID(12.5)} 0px ${GRID(17.5)};
-  padding: 0px 0px 0px ${GRID(10.5)};
-  width: 100vw;
-  @media (max-width: 768px) {
-    margin: ${GRID(15)} 0px ${GRID(5.75)};
-    padding: 0px ${GRID(2)};
-  }
-`;
+const Section = styled.section`
+    margin: ${GRID(1)} 0;
+
+    @media only screen and (min-width: 1024px) {
+        margin: ${GRID(2)} 0;
+    }
+`
+
+const SectionWrapper = styled.section`
+    margin: 0 auto;
+    padding: ${GRID(8)} ${GRID(2)};
+    max-width: ${GRID(180)};
+    
+    @media only screen and (min-width: 1024px) {
+        padding: ${GRID(10)} ${GRID(9)};
+    }
+`
 
 const Subtitle = styled.h3`
   color: ${COLORS.GREEN_MAIN};
