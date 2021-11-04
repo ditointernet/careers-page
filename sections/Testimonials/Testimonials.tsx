303c13b0518504a24b6b69746828438479e1a84a
Diego Roso da Silva <diego.silva@dito.com.br>
2021-11-04 19:01:32 -0300

    include photos

--- sections/Testimonials/Testimonials.tsx
+++ sections/Testimonials/Testimonials.tsx
@@ -10,13 +10,20 @@ import "swiper/css/pagination"
 import "swiper/css/navigation"
 import "swiper/css/effect-cards"
 
+enum Area {
+    TECNOLOGIA = "tecnologia",
+    COMERCIAL = "comercial",
+    GENTEEGESTAO = "gente & gestão",
+}
+
 type Slide = {
     image: string,
     video: string,
     name: string,
     profession: string,
-    local: string,
     testimonial: string,
+    area: Area,
+    link: string,
 }
 
 
@@ -28,70 +35,81 @@ const Testimonials = () => {
 
     const slideItens: Slide[] = [
         {
-            image: "./images/temp_testimonials.png",
-            video: "https://player.vimeo.com/video/640017082?h=5190752281",
+            area: Area.COMERCIAL,
             name: "Isabella Moema",
-            profession: "Especialista de Sucesso do Cliente",
-            local: "Belo Horizonte, MG",
-            testimonial: "\"Eu sou sempre muito incentivada aqui dentro a ser a pessoa que eu sou, ...\"",
+            image: "./images/feras/isabella-moema.jpg",
+            video: "https://player.vimeo.com/video/640017082?h=5190752281",
+            profession: "Especialista de sucesso do cliente",
+            testimonial: "\"Eu vim de outras startups onde essa questão da cultura é bem enraizada, mas eu nunca vivi, de fato, uma cultura igual a gente vive aqui na Dito, no dia a dia.\"",
+            link: "https://dito.hire.trakstar.com/?team_id=5436&q=&limit=25#content",
         },
         {
-            image: "./images/temp_testimonials.png",
-            video: "https://player.vimeo.com/video/640030966?h=27fac8eef8",
+            area: Area.COMERCIAL,
             name: "Luis Sena",
-            profession: "Consultor de Negócios",
-            local: "",
-            testimonial: "",
+            image: "./images/feras/luis-sena.jpg",
+            video: "https://player.vimeo.com/video/640030966?h=27fac8eef8",
+            profession: "Consultor de negócios",
+            testimonial: "\"Eu indicaria a Dito para qualquer pessoa que, de fato, queira entregar algo relevante para o mercado. Se a pessoa está disposta a estudar, a fazer boas entregas, a Dito é o melhor lugar.\"",
+            link: "https://dito.hire.trakstar.com/?team_id=5436&q=&limit=25#content",
         },
         {
-            image: "./images/temp_testimonials.png",
-            video: "https://player.vimeo.com/video/640051678?h=c4ffd2f1aa",
+            area: Area.TECNOLOGIA,
             name: "Luciana Batista",
-            profession: "Product Manager",
-            local: "",
-            testimonial: "",
+            image: "./images/feras/luciana-batista.jpg",
+            video: "https://player.vimeo.com/video/640051678?h=c4ffd2f1aa",
+            profession: "Product manager",
+            testimonial: "\"O ambiente aqui é muito saudável. A rotina aqui é muito fluida, a gente consegue se comunicar com qualquer pessoa.\"",
+            link: "https://dito.hire.trakstar.com/?team_id=8496&team_id=5567&q=&limit=25#content",
         },
         {
-            image: "./images/temp_testimonials.png",
-            video: "https://player.vimeo.com/video/640071139?h=0b52cee9cb",
+            area: Area.TECNOLOGIA,
             name: "Victória Villas Boas",
+            image: "./images/feras/victoria-villas-boas.jpg",
+            video: "https://player.vimeo.com/video/640071139?h=0b52cee9cb",
             profession: "Desenvolvedora",
-            local: "",
-            testimonial: "",
+            testimonial: "\"A Dito tem muita gente que está aqui há anos e é muito difícil você encontrar isso em outras empresas de tecnologia. Isso que encontrei aqui: muita gente muito boa e que não deixa a Dito de jeito nenhum.\"",
+            link: "https://dito.hire.trakstar.com/?team_id=8496&team_id=5567&q=&limit=25#content",
         },
         {
-            image: "./images/temp_testimonials.png",
-            video: "https://player.vimeo.com/video/640153879?h=69e193e966",
+            area: Area.GENTEEGESTAO,
             name: "Luiza Rezende",
-            profession: "Analista de Gestão de Pessoas",
-            local: "",
-            testimonial: "",
+            image: "./images/feras/luiza-rezende.jpg",
+            video: "https://player.vimeo.com/video/640153879?h=69e193e966",
+            profession: "Analista de gestão de pessoas",
+            testimonial: "\"O trabalho remoto trouxe pra gente pessoas dos mais diversos panoramas, experiências profissionais. E isso é muito rico, isso traz ideias mais interessantes, mais completas, com muito mais potencial.\"",
+            link: "https://dito.hire.trakstar.com/?team_id=26339&team_id=27867&q=&limit=25#content",
         },
         {
-            image: "./images/temp_testimonials.png",
-            video: "https://player.vimeo.com/video/640173575?h=62716d8b85",
+            area: Area.TECNOLOGIA,
             name: "Pedro Rezende",
+            image: "./images/feras/pedro-rezende.jpg",
+            video: "https://player.vimeo.com/video/640173575?h=62716d8b85",
             profession: "Desenvolvedor",
-            local: "",
-            testimonial: "",
+            testimonial: "\"Aprendi muita coisa com pessoas que não têm nada a ver com a área. Não importa a área, todas elas se comunicam muito bem e ensinam coisas umas para as outras.\"",
+            link: "https://dito.hire.trakstar.com/?team_id=8496&team_id=5567&q=&limit=25#content",
         },
     ];
 
     const SwiperItens = slideItens.map((slide: Slide, index: number) => (
         <SwiperSlide key={index}>
-            <MidiaContainer
-                image={slide.image}
-                onClick={() => {
-                    setModalOpen(!modalOpen);
-                    setIframeVideo(slide.video);
-                }}
-            />
+            <MidiaContainer image={slide.image} />
             <Wrapper>
-                <Name>{slide.name}</Name>
-                <Profession>{slide.profession}</Profession>
-                <Local>{slide.local}</Local>
-                <Testimonial>{slide.testimonial}</Testimonial>
-                <ButtonSecondary>Ver vagas de tecnologia</ButtonSecondary>
+                <div>
+                    <Name>{slide.name}</Name>
+                    <Description>{slide.profession}</Description>
+                    <Description>{slide.testimonial}</Description>
+                </div>
+                <div>
+                    <ButtonVideo
+                        onClick={() => {
+                            setModalOpen(!modalOpen);
+                            setIframeVideo(slide.video);
+                        }}
+                    >
+                        Assista o vídeo
+                    </ButtonVideo>
+                    <ButtonSecondary target="_black" href={slide.link}>Ver vagas de {slide.area}</ButtonSecondary>
+                </div>
             </Wrapper>
         </SwiperSlide>
     ))
@@ -132,11 +150,9 @@ const Testimonials = () => {
 }
 
 const Section = styled.section`
-    // section disabled temporary for waiting approve content
-    /* display: none; */
     margin: ${GRID(1)} 0;
   
-    @media only screen and (min-width: 1024px) {
+    @media only screen and (min-width: 1200px) {
         margin: ${GRID(2)} 0;
     }
 `
@@ -150,7 +166,7 @@ const SectionWrapper = styled.div`
     flex-direction: column;
     justify-content: center;
   
-    @media only screen and (min-width: 1024px) {
+    @media only screen and (min-width: 1200px) {
         padding: ${GRID(10)} ${GRID(9)};
     }
 `
@@ -162,7 +178,7 @@ const Title = styled.h2`
     line-height: ${GRID(4.125)};
     font-weight: 700;
 
-    @media only screen and (min-width: 1024px) {
+    @media only screen and (min-width: 1200px) {
         margin: 0;
         font-size: ${GRID(4.5)};
         line-height: ${GRID(6.75)};
@@ -171,9 +187,9 @@ const Title = styled.h2`
 
 const SwiperMobile = styled(Swiper)`
     width: ${GRID(39.625)};
-    height: ${GRID(55.125)};
+    height: ${GRID(66.875)};
 
-    @media only screen and (min-width: 1024px) {
+    @media only screen and (min-width: 1200px) {
         display: none;
     }
 
@@ -191,12 +207,12 @@ const SwiperMobile = styled(Swiper)`
 `
 
 const SwiperDesktop = styled(Swiper)`
-    width: calc(100% - ${GRID(3.75)});
+    width: 100%;
     display: none;
     padding: ${GRID(7.5)};
-    max-width: ${GRID(129.125)};
+    max-width: ${GRID(131.625)};
 
-    @media only screen and (min-width: 1024px) {
+    @media only screen and (min-width: 1200px) {
         display: block;
     }
 
@@ -250,23 +266,15 @@ const MidiaContainer = styled.div<{ image: string }>`
     width: 100%;
     cursor: pointer;
     height: ${GRID(17)};
-    display: flex;
-    align-items: center;
     background-size: cover;
-    justify-content: center;
     background-image: url(${props => props.image});
+    background-position-y: -20px;
 
-    @media only screen and (min-width: 1024px) {
+    @media only screen and (min-width: 1200px) {
         width: ${GRID(47.25)};
         height: 100%;
-    }
-
-    &::before {
-        width: ${GRID(11.625)};
-        height: ${GRID(11.625)};
-        content: '';
-        background-size: cover;
-        background-image: url('./icons/play.svg');
+        border-top-left-radius: ${GRID(3)};
+        border-bottom-left-radius: ${GRID(3)};
     }
 `
 
@@ -275,10 +283,14 @@ const Wrapper = styled.div`
     height: calc(100% - ${GRID(17)});
     padding: ${GRID(3)} ${GRID(2.5)};
 
-    @media only screen and (min-width: 1024px) {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+
+    @media only screen and (min-width: 1200px) {
         width: calc(100% - ${GRID(47.25)});
         height: 100%;
-        padding: ${GRID(7)} ${GRID(6)};
+        padding: ${GRID(7)};
     }
 `
 
@@ -288,45 +300,48 @@ const Name = styled.p`
     font-size: ${GRID(2.75)};
     font-weight: 700;
     line-height: ${GRID(4.125)};
-    margin-bottom: ${GRID(2)};
 
-    @media only screen and (min-width: 1024px) {
+    @media only screen and (min-width: 1200px) {
         font-size: ${GRID(3.75)};
         line-height: ${GRID(5.625)};
-        margin-bottom: ${GRID(1.5)};
     }
 `
 
-const DescriptionDefault = styled.p`
+const Description = styled.p`
     margin: 0;
     color: ${COLORS.GRAY_4};
     font-size: ${GRID(2)};
     font-weight: 500;
     line-height: ${GRID(3)};
-`
-
-const Profession = styled(DescriptionDefault)`
     margin-bottom: ${GRID(1.5)};
 
-    @media only screen and (min-width: 1024px) {
-        margin-bottom: ${GRID(0.5)};
+    @media only screen and (min-width: 1200px) {
+        margin-bottom: ${GRID(3)};
     }
 `
 
-const Local = styled(DescriptionDefault)`
-    margin-bottom: ${GRID(3)};
-
-    @media only screen and (min-width: 1024px) {
-        margin-bottom: ${GRID(4.5)};
-    }
-`
-
-const Testimonial = styled(DescriptionDefault)`
-    max-width: ${GRID(41.25)};
+const ButtonVideo = styled.div`
+    gap: ${GRID(1.25)};
+    width: ${GRID(18.125)};
+    color: ${COLORS.GREEN_MAIN};
+    cursor: pointer;
+    display: flex;
+    padding: ${GRID(1)} ${GRID(1.25)};
+    font-size: ${GRID(1.5)};
+    font-weight: 500;
+    align-items: center;
+    line-height: ${GRID(2.25)};
     margin-bottom: ${GRID(2)};
+    justify-content: center;
 
-    @media only screen and (min-width: 1024px) {
-        margin-bottom: ${GRID(6)};
+    &::before {
+        width: ${GRID(3)};
+        height: ${GRID(3)};
+        content: '';
+        display: inline-block;
+        background-size: cover;
+        background-image: url("./icons/video.svg");
+        background-repeat: no-repeat;
     }
 `
 
