30cbf8c65a74a708564be7cfd7bf67089a5956d2
Kaio Maia <kaio.maia@dito.com.br>
2023-08-28 18:43:06 -0300

    indenting code

--- utils/colors-utils.ts
+++ utils/colors-utils.ts
@@ -1,17 +1,17 @@
 import { COLORS_NEXT } from "@ditointernet/uai-foundation";
 
 function hexToRGB(hex: string) {
-    if (hex === undefined) return COLORS_NEXT.WHITE;
+  if (hex === undefined) return COLORS_NEXT.WHITE;
   
-    hex = hex[0] === "#" ? hex.slice(1) : hex;
-    const bigint = parseInt(hex, 16);
-    const r = (bigint >> 16) & 255;
-    const g = (bigint >> 8) & 255;
-    const b = bigint & 255;
+  hex = hex[0] === "#" ? hex.slice(1) : hex;
+  const bigint = parseInt(hex, 16);
+  const r = (bigint >> 16) & 255;
+  const g = (bigint >> 8) & 255;
+  const b = bigint & 255;
   
-    return r + ", " + g + ", " + b;
-  }
+  return r + ", " + g + ", " + b;
+}
   
-  export function transparentize(color: string, alpha: string) {
-    return `rgba(${hexToRGB(color)}, ${alpha})`;
-  }
+export function transparentize(color: string, alpha: string) {
+  return `rgba(${hexToRGB(color)}, ${alpha})`;
+}
